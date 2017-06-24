7b7c00dc7eff1dcc315d71822044354550952100
Fred Trinh <fred@freds-mbp.lan>
2017-06-24 13:31:28 -0500

    Initial Commit

---
+++ Program_Six/main.cpp
@@ -0,0 +1,41 @@
+/*
+ 
+ DESCRIPTION:    PROGRAM SIX
+ 
+ CLASS:          COSC 1436.3S1
+ 
+ AUTHOR:         FRED TRINH
+ 
+ DATE:           27 JUNE 2017
+ 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+    return 0;
+}
+
+/*
+
+Write a program, which will ask for and read in a line of text characters.
+The program will then determine if the line of text forms a palindrome and print whether it is or not.
+After printing this message, the program will ask for another line of text.
+The program will end when the line of text read matches the string “END” (letters in all caps).
+
+1) Use arrays of char type. Programs using the string class will not be accepted.
+2) When testing if the string is a palindrome, an upper and lower case character will be considered a match.
+   Only when checking for END is there a requirement for the case to be specifically upper case.
+3) The maximum number of displayable characters in a line will be 80.
+
+1) An electronic copy of the .cpp file in the project folder as created by Visual Studio.
+   This will be emailed to the instructor with the subject line “COSC 1436.3S1 – Lab 6”.
+2) If you wish any feedback on your work, turn in a printed listing of the .cpp file that you created.
+
+BONUS: Do the program without use of any “str” methods – plus 10 points.
+
+*/
